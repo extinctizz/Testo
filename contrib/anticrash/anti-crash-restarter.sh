cbb1fe6a397a8bcc4d15cd167741d92173c8a37d
extinctizz <raineyethan@gmail.com>
2020-05-20 21:06:08 -0500

    test

--- contrib/anticrash/anti-crash-restarter.sh
+++
@@ -1,6 +0,0 @@
-# anticrash config
-export ANTICRASH_LOGFILE=/path/to/anticrash/trace.log
-export ANTICRASH_HANDLER=/path/to/anticrash/handler.sh
-export ANTICRASH_ENABLE=1
-export COREDUMP_HANDLER=/path/to/anticrash/coredump.sh
-# line to add to restarter
